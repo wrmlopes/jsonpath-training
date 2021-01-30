dec06c3e772abf985d66e5570958cd8bfd91d96c
F9901059 Wilson Ricardo Martins Lopes <wrmlopes@bb.com.br>
2021-01-30 20:21:59 -0300

    ajustes na apresentaçao de resultados cada elemento da array em uma linha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JSONPath } from 'jsonpath-plus';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jsonpath-training';
+
+  pathExpression: string = '';
+
+  inputSample = `{
+    "store": {
+      "book": [
+        {
+          "category": "reference",
+          "author": "Nigel Rees",
+          "title": "Sayings of the Century",
+          "price": 8.95
+        },
+        {
+          "category": "fiction",
+          "author": "Evelyn Waugh",
+          "title": "Sword of Honour",
+          "price": 12.99
+        },
+        {
+          "category": "fiction",
+          "author": "Herman Melville",
+          "title": "Moby Dick",
+          "isbn": "0-553-21311-3",
+          "price": 8.99
+        },
+        {
+          "category": "fiction",
+          "author": "J. R. R. Tolkien",
+          "title": "The Lord of the Rings",
+          "isbn": "0-395-19395-8",
+          "price": 22.99
+        }
+      ],
+      "bicycle": {
+        "color": "red",
+        "price": 19.95
+      }
+    }
+  }`;
+
+  resultText: string;
+
+  setResultText(value) {
+    let arrayResult = JSONPath({ path: this.pathExpression, json: JSON.parse(this.inputSample) });
+    this.resultText = '';
+
+    arrayResult.forEach(element => {
+      this.resultText += JSON.stringify(element) + "\n";
+      
+    });
+  }
+
+
+  testExpression(event) {
+    this.pathExpression = event.target.value ?? '$..';
+    this.setResultText(this.pathExpression);
+  }
+
+
+
 }
